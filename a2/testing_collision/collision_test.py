d70a4c2e73f634babc0ed20170b8f3c80a6a4e74
Aaron Zhang <aaronz@live.unc.edu>
2017-10-22 12:32:34 -0400

    finished

--- a2/testing_collision/collision_test.py
+++ a2/testing_collision/collision_test.py
@@ -3,52 +3,64 @@ import os
 import hashlib
 import subprocess
 
-STUDENT_FILES = "./StudentFiles_collision"
+STUDENT_FILES = "./student_submissions"
+grades = {}
 def main():
-    filesToTest = runOverFilesInLocalDirectory(STUDENT_FILES)
-    testFilesCollision(filesToTest)
+    studentDirectoryList = getStudentDirectoryList()
+    runTest(studentDirectoryList)
+    with open("collision_grades.json",'w') as fileToWrite:
+        json.dump(grades,fileToWrite)
 
+def getStudentDirectoryList():
+    studentDirectories = []
+    for root, dirs, files in os.walk(STUDENT_FILES):
+        studentDirectories = dirs
+        break
+    return studentDirectories
 
-def testFilesCollision(filesToRun):
-    for fileName in filesToRun:
-        print(fileName)
-        studentOnyen = fileName.split('_')
-        hashVals = getHashValues(fileName)
-        if (hashVals[0]=="Timeout") or (hashVals[1]=="Error"):
-            print("%s program had an error running their program."%studentOnyen[1])
+def runTest(studentDirectories):
+    for studentDir in studentDirectories:
+        submissionFolder = "%s/%s/Submission attachment(s)"%(STUDENT_FILES,studentDir)
+        temp = os.listdir(submissionFolder)
+        studentOnyen = studentDir[studentDir.find('(')+1:studentDir.find(')')]
+        print(studentOnyen)
+        if(len(temp)==0):
+            #students get 0
+            grades[studentOnyen] = 0
         else:
-            hashVals = hashVals[0].split(' ') #delim on space
-            print(hashVals)
-            hashToSameValue_first24Bits = collisionTest(hashVals[0],hashVals[1])
-            print("%s %s"%(studentOnyen[1],hashToSameValue_first24Bits))
+            preImageTest(submissionFolder,studentOnyen)
 
-def getHashValues(fileName):
-    proc = subprocess.Popen(["python3","%s/%s"%(STUDENT_FILES,fileName)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def preImageTest(submissionFolder,onyen):
+    proc = subprocess.Popen(["python3","%s/a2_%s_collision.py"%(submissionFolder,onyen)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
         output, errs = proc.communicate(timeout=15)
     except subprocess.TimeoutExpired:
         proc.kill()
         output, errs = proc.communicate()
-        return ["Timeout","Timeout"]
+        grades[onyen] = 0
+        return
     except Exception as e:
         proc.kill()
         output, errs = proc.communicate()
-        return ["Error",e]
-    #If output is valid, decode and strip newline
+        grades[onyen] = 0
+        return
     output = output.decode('UTF-8').rstrip()
-    errs = errs.decode('UTF-8').rstrip()
-    returnTupule = [output,errs]
-    return returnTupule
+    hashVals = output.split(' ') #delim on space
+    if len(hashVals)<2:
+        grades[onyen] = 0
+        return
+    hashToSameValue_first24Bits = collisionTest(hashVals[0],hashVals[1])
+    if hashToSameValue_first24Bits:
+        grades[onyen] = 1
+    else:
+        grades[onyen] = 0
 
 def collisionTest(hash1, hash2):
     t1 = hashlib.sha256(hash1.encode('utf-8')).hexdigest()
     t2 = hashlib.sha256(hash2.encode('utf-8')).hexdigest()
     return (t1[:6]==t2[:6])
 
-def runOverFilesInLocalDirectory(directory):
-    tempList = [];
-    for fileName in os.listdir(directory):
-        tempList.append(fileName)
-    return tempList
-
+def toSixChar(inputStr):
+    return inputStr[:6]
 main()
+#####################
--- a2/testing_pre_image/preimage_test.py
+++ a2/testing_pre_image/preimage_test.py
@@ -60,36 +60,3 @@ def toSixChar(inputStr):
 
 main()
 #####################
-
-
-
-
-def a():
-    filesToTest = runOverFilesInLocalDirectory(STUDENT_FILES)
-    hashVal = toSixChar(hashlib.sha256(TEST_STR.encode('utf-8')).hexdigest())
-    preImageTest(hashVal,filesToTest)
-
-def b(hashVal, filesToTest):
-    for studentFile in filesToTest:
-        proc = subprocess.Popen(["python3","%s/%s"%(STUDENT_FILES,studentFile),hashVal], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        try:
-            output, errs = proc.communicate(timeout=300)
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            output, errs = proc.communicate()
-            #student failed
-            continue
-        output = output.decode('UTF-8').rstrip()
-        studentHashVal = hashlib.sha256(output.encode('utf-8')).hexdigest()
-        studentHashVal = toSixChar(studentHashVal)
-        if studentHashVal==hashVal:
-            #students solution is correct
-            print("thing")
-        else:
-            #student failed
-            print("some")
-def c(directory):
-    tempList = [];
-    for fileName in os.listdir(directory):
-        tempList.append(fileName)
-    return tempList
